e85a0680e15216b014ba2ea3a66fa73a499993e0
imvkavi <mailmekavishmg@gmail.com>
2021-04-13 22:27:34 +0530

    commit

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-print('hello')
-print('hello students')
+#mad lib generator
+loop=1
+while loop<10:
+    noun=input('choose a noun:')
+    pnoun=input('choose a plural noun:')
+    noun2=input('choose another noun:')
+    place=input('enter a place:')
+    adj=input('enter an adjective:')
+    noun3=input('enter another noun:')
+    #displays a story based on user input
+
+    print('..............................')
+    print()
